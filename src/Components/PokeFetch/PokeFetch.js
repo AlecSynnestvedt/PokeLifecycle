0cebd2cca66c1a6585171b5a9d4b1b2be56212c5
Alec Synnestvedt <aleccsyn@gmail.com>
2021-05-10 08:05:44 -0400

    closer to correct

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,10 +9,15 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+
     }
+    //timer should go here probably, or maybe below the next } but definitely before the fetch()
   }
 
   fetchPokemon() {
+    setState{
+      clockOn: true
+    }
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
@@ -28,14 +33,36 @@ class PokeFetch extends Component {
       })
       .catch((err) => console.log(err))
   }
-
+  //Timer
+ timer = () => {
+    if (this.state.clock > 0) {
+      this.setState(
+        {clock: this.state.clock -1}
+      )
+    } else {
+      clearInterval();
+        this.setState(
+          {clockOn: false }
+        )
+    }
+  } 
+  
+  
+  
+  //Lifecycle method
+  componentDidUpdate () { //Lifecycle method here checking that the timer ahs updated before proceeding down
+    setInterval(() => {this.timer()}, 1000)//10000 ms = 10 s 
+  }
+  
   render() {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <h1 className={'timer'} >Timer Display {this.state.clockOn ? this.state.clock : null} </h1>
+        
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
+          {this.state.clockOn ? <img className={'pokeImg'} src={this.state.pokeSprite} />
+          //I think there are a few ways to do this, but my best option looks like it is likely to filter with CSS and toggle that on / off w/ onClic.. 
           <h1 className={'pokeName'}>{this.state.pokeName}</h1>
         </div>
       </div>
